f7de1c9f0c450281947e068e9004ca7b38e5e45a
Axel Reyes <axel.reyes.b@uni.pe>
2026-05-07 13:56:57 -0500

    Avance de la semana 4

---
+++ Libreria_cc232/build-debug/Semana4/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/AXEL/OneDrive/Escritorio/uni/2026-1/AED/Repositorio/Personal/CC232/Libreria_cc232/Semana4
+# Build directory: C:/Users/AXEL/OneDrive/Escritorio/uni/2026-1/AED/Repositorio/Personal/CC232/Libreria_cc232/build-debug/Semana4
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[semana4_public]=] "C:/Users/AXEL/OneDrive/Escritorio/uni/2026-1/AED/Repositorio/Personal/CC232/Libreria_cc232/build-debug/Semana4/sem4_test_public.exe")
+set_tests_properties([=[semana4_public]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/AXEL/OneDrive/Escritorio/uni/2026-1/AED/Repositorio/Personal/CC232/Libreria_cc232/Semana4/CMakeLists.txt;38;add_test;C:/Users/AXEL/OneDrive/Escritorio/uni/2026-1/AED/Repositorio/Personal/CC232/Libreria_cc232/Semana4/CMakeLists.txt;0;")
+add_test([=[semana4_internal]=] "C:/Users/AXEL/OneDrive/Escritorio/uni/2026-1/AED/Repositorio/Personal/CC232/Libreria_cc232/build-debug/Semana4/sem4_test_internal.exe")
+set_tests_properties([=[semana4_internal]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/AXEL/OneDrive/Escritorio/uni/2026-1/AED/Repositorio/Personal/CC232/Libreria_cc232/Semana4/CMakeLists.txt;39;add_test;C:/Users/AXEL/OneDrive/Escritorio/uni/2026-1/AED/Repositorio/Personal/CC232/Libreria_cc232/Semana4/CMakeLists.txt;0;")
